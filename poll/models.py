58ae3ffd1306b4347bd4a3d57569fb3e8bc47b4a
raptor72 <mctaras51@yandex.ru>
2019-03-10 22:16:48 +0400

    Add Class Question

--- poll/models.py
+++ poll/models.py
@@ -9,10 +9,21 @@ class Poll(models.Model):
     comment = models.TextField(blank=True, db_index=True)
     date_create = models.DateTimeField(auto_now_add=True)
     is_public = models.BooleanField(default=False)
+    questions = models.ManyToManyField('Question', blank=True, related_name='polls')
 
     def get_absolute_url(self):
         return reverse('poll_detail_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return '{}'.format(self.title)
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=150)
+    case = models.CharField(max_length=150, blank=True)
+    answer = models.CharField(max_length=150, blank=True)
+
+    def __str__(self):
+        return '{}'.format(self.text)
+
 
