8c12ce5c06dfdec8588f988c34adbacf8fd0fdb9
raptor72 <mctaras51@yandex.ru>
2019-03-12 19:10:00 +0400

    Add class Mixin

--- poll/urls.py
+++ poll/urls.py
@@ -6,5 +6,5 @@ from .views import *
 urlpatterns = [
     path('', polls_list, name='polls_list_url'),
 #    path('poll/<str:slug>/', poll_detail, name = 'poll_detail_url')
-    path('<str:slug>/', poll_detail, name = 'poll_detail_url')
+    path('<str:slug>/', PollDetail.as_view(), name = 'poll_detail_url')
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
 
 from .models import Poll, Question
+from .utils import ObjectDetailMixin
 
 # Create your views here.
 
@@ -8,7 +11,8 @@ def polls_list(request):
     polls = Poll.objects.all()
     return render(request, 'poll/index.html', context={'polls': polls})
 
+class PollDetail(ObjectDetailMixin, View):
+    model = Poll
+    template = 'poll/poll_detail.html'
 
-def poll_detail(request, slug):
-    poll = Poll.objects.get(slug__iexact=slug)
-    return render(request, 'poll/poll_detail.html', context={'poll': poll})
+
