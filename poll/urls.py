1498ba151aed8dce3a006c430554cbd283475193
raptor72 <mctaras51@yandex.ru>
2019-03-21 19:03:11 +0400

    Use IndexView, LoginView, LogoutView

--- poll/urls.py
+++ poll/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from .views import *
 
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', polls_list, name='polls_list_url'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
+    path('', IndexView.as_view(), name='polls_list_url'),
+    path('login/', LoginView.as_view(template_name='poll/login.html')),
+    path('logout/', LogoutView.as_view()),
     path('user_results/', user_results, name='user_results'),
     path('<str:slug>/', PollDetail.as_view(), name='poll_detail_url'),
     path('<str:slug>/result/', poll_result, name='poll_result'),
--- poll/views.py
+++ poll/views.py
@@ -6,33 +6,18 @@ from .utils import PollDetailMixin
 
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+#from django.urls import reverse
 from django.contrib import messages
 
-
-def polls_list(request):
-    polls = Poll.objects.all()
-    return render(request, 'poll/index.html', context={'polls': polls})
+from django.views import generic
 
 
-def login_user(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse('polls_list_url'))
-        else:
-            messages.error(request, 'uncorrected name or password')
-    return render(request, 'poll/login.html', {})
+class IndexView(generic.ListView):
+    template_name='poll/index.html'
+    context_object_name='polls'
 
-
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('polls_list_url'))
+    def get_queryset(self):
+        return Poll.objects.all()
 
 
 class PollDetail(LoginRequiredMixin, PollDetailMixin, View):
