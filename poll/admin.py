84d7ce499b25c28bca010e55db6e66bb72e9d7d9
raptor72 <mctaras51@yandex.ru>
2019-04-01 21:38:27 +0400

    Add User Votes in admin

--- poll/admin.py
+++ poll/admin.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 
 import nested_admin
 
-from .models import Poll, Question, Choice, Vote
+from .models import Poll, Question, Choice, Vote, User
 
-
-#admin.site.register(Poll)
-#admin.site.register(Question)
-#admin.site.register(Choice)
-#admin.site.register(Vote)
 
 class ChoiceInline(nested_admin.NestedTabularInline):
     model = Choice
@@ -19,12 +14,22 @@ class QuestionInline(nested_admin.NestedStackedInline):
     model = Question
     extra = 1
     sortable_field_name = "order"
-    inlines = [ChoiceInline,]
+    inlines = [ChoiceInline]
 
 
 class TableofChoiceAdmin(nested_admin.NestedModelAdmin):
     inlines = [QuestionInline]
 
 
+class VoteInline(admin.TabularInline):
+    model = Vote
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [VoteInline]
+
+
 admin.site.register(Poll, TableofChoiceAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
